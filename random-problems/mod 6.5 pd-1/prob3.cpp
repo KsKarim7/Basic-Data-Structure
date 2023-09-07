5eaac54f398b8b9aa99395c129e845c1eaa94ee0
Khaled Karim <kskarim177@gmail.com>
2023-09-07 13:46:31 +0600

    getting on form

--- random-problems/mod 6.5 pd-1/prob3.cpp
+++ random-problems/mod 6.5 pd-1/prob3.cpp
@@ -13,8 +13,63 @@ public:
     }
 };
 
+void insert_list(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void who_is_in_the_middle(Node *head, int len)
+{
+    // if(len % 2 == 2){
+
+    // }
+    Node *temp = head;
+    for (int i = 1; i <= len; i++)
+    {
+        if (len % 2 == 0)
+        {
+            if (i == len / 2)
+            {
+                cout << temp->val << " " << temp->next->val << " " << endl;
+            }
+        }
+        else
+        {
+            if (i == len / 2)
+            {
+                cout << temp->next->val << endl;
+            }
+        }
+        temp = temp->next;
+    }
+}
+
 int main()
 {
-
+    Node *head = NULL;
+    int len = 0;
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        len++;
+        insert_list(head, val);
+    }
+    who_is_in_the_middle(head, len);
     return 0;
 }
