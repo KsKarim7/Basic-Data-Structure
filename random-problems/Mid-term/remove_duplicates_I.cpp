c4b2f57ccb078073483c81184386facfdd2e2417
Khaled Karim <kskarim177@gmail.com>
2023-09-21 02:42:13 +0600

    good night for now

---
+++ random-problems/Mid-term/remove_duplicates_I.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (head != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        while (val == -1)
+        {
+            break;
+        }
+        insert_tail(head, tail, val);
+    }
+    // remove_duplicates(head,tail);
+    print_list(head);
+    return 0;
+}
