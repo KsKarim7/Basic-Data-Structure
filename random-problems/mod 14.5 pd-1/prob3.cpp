4bb8bde4ee11a372ec2a320982b7148dca2803f1
Khaled Karim <82402301+KsKarim7@users.noreply.github.com>
2023-09-30 13:36:18 +0600

    Update prob3.cpp

---
+++ random-problems/mod 14.5 pd-1/prob3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int>st;
+    stack<int>stt;
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+    while(!st.empty()){
+        // cout<<st.top()<<" ";
+        stt.push(st.top());
+        st.pop();
+    }
+    while(!stt.empty()){
+        cout<<stt.top()<<" ";
+        stt.pop();
+    }
+   
+    return 0;
+}
+    
