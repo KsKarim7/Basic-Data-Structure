51aa89012e8859fb15336e5a87e017a92f23d2b3
Khaled Karim <82402301+KsKarim7@users.noreply.github.com>
2023-09-30 13:35:39 +0600

    Update prob2.cpp

---
+++ random-problems/mod 14.5 pd-1/prob2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int>st;
+    queue<int>q;
+    bool flag = true;
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        int x;
+        cin>>x;
+        q.push(x);
+    }
+    while(!st.empty()){
+        if(size(st) != size(q)){
+            flag = false;
+            break;
+        }
+        if(st.top() != q.front()){
+            flag = false;
+            break;
+        }
+        q.pop();
+        st.pop();
+    }
+    if(flag == true){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
+    
