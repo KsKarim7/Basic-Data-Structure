847c2610698b0d90babfe9068eea0718f9fec741
Khaled Karim <kskarim177@gmail.com>
2023-10-17 09:15:51 +0600

    trying hard

--- random-problems/Assignment 3/specialQueries.cpp
+++ random-problems/Assignment 3/specialQueries.cpp
@@ -15,27 +15,41 @@ public:
     }
     // int front()
     // {
-    // return l.begin();
+    //     return l.begin();
     // }
+    bool empty()
+    {
+        return l.empty();
+    }
 };
 
 int main()
 {
     myQueue q;
-    string n;
+    int n;
     cin >> n;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int x;
-    //     cin >> x;
-    //     if (x == 0)
-    //     {
-    //         string name;
-    //         cin >>
-    //     }
-    // }
-    q.push(n);
-    q.push("memo");
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            string s;
+            cin >> s;
+            q.push(s);
+        }
+        else if (q.empty())
+        {
+            cout << "Invalid";
+        }
+        else
+        {
+            // q.front();
+            q.pop();
+        }
+    }
+    // q.push(n);
+    // q.push("memo");
     // cout << q.front() << " ";
     return 0;
 }
