e90d7e5386f40fa36ce11789f42a25d441ec5439
Khaled Karim <kskarim177@gmail.com>
2023-11-08 23:42:32 +0600

    keeping the streak

---
+++ random-problems/module 18.5 pd-1 (coding ninja)/count_leafNodes.cpp
@@ -0,0 +1,41 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+
+***********************************************************/
+
+// vector<int>v;
+
+// int count_leaf(BinaryTreeNode<int> *root){
+//     if(root == NULL){
+//         return 0;
+//     }
+//     if(root->left == NULL && root->right == NULL){
+//         return 1;
+//     }
+//     else{
+//         int l = count_leaf(root->left);
+//         int r = count_leaf(root->right);
+//         return l+r;
+//     }
+// }
+
+// int noOfLeafNodes(BinaryTreeNode<int> *root){
+//     // Write your code here.
+//     v.clear();
+//     cout<<count_leaf(root)<<endl;
+// }
---
+++ random-problems/module 18.5 pd-1 (coding ninja)/left_sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+/*
+    Tree Node class.
+
+    class BinaryTreeNode
+    {
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    }
+*/
+
+// vector<int>v;
+
+// int left_sum(BinaryTreeNode<int> *root){
+// 	int lsum = 0;
+//     if(root == NULL){
+//         return 0;
+//     }
+// 	lsum += root->data;
+// 	left_sum(root->left);
+// 	return lsum;
+// }
+
+// long long leftSum(BinaryTreeNode<int> *root)
+// {
+// 	// Write your code here.
+// 	v.clear();
+// 	cout<<left_sum(root)<<endl;
+
+// }
