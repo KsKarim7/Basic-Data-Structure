75492c8116d700373b940647f3af0f70ca138e43
Khaled Karim <kskarim177@gmail.com>
2023-11-16 13:23:35 +0600

    2 more to go

---
+++ random-problems/Final Exam/Give_current_max.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+    Student(string name, int roll, int marks)
+    {
+        this->marks = marks;
+        this->roll = roll;
+        this->name = name;
+    }
+};
+
+class compare
+{
+public:
+    bool operator()(Student a, Student b)
+    {
+        if (a.marks < b.marks)
+            return true;
+        else if (a.marks > b.marks)
+            return false;
+        else
+        {
+            if (a.roll > b.roll)
+                return true;
+            else
+                return false;
+        }
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    priority_queue<Student, vector<Student>, compare> pq;
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        int roll, marks;
+        cin >> name >> roll >> marks;
+        Student obj(name, roll, marks);
+        pq.push(obj);
+    }
+    // while (!pq.empty())
+    // {
+    //     cout << pq.top().name << " " << pq.top().roll << " " << pq.top().marks << endl;
+    //     pq.pop();
+    // }
+
+    cin >> n;
+    while (n--)
+    {
+        int q;
+        cin >> q;
+        if (q == 0)
+        {
+            string name;
+            int roll, marks;
+            cin >> name >> roll >> marks;
+            Student obj(name, roll, marks);
+            pq.push(obj);
+            cout << pq.top().name << " " << pq.top().roll << " " << pq.top().marks << endl;
+        }
+        else if (pq.empty())
+        {
+            cout << "Empty" << endl;
+        }
+        else if (q == 1)
+        {
+            cout << pq.top().name << " " << pq.top().roll << " " << pq.top().marks << endl;
+        }
+        else if (q == 2)
+        {
+            pq.pop();
+            if (pq.empty())
+            {
+                cout << "Empty" << endl;
+            }
+            else
+            {
+                cout << pq.top().name << " " << pq.top().roll << " " << pq.top().marks << endl;
+            }
+        }
+    }
+    return 0;
+}
